a5c742185f6a6e483113c4dd11e898cfb364195d
princevishwa <35019342+princevishwa@users.noreply.github.com>
2018-02-17 20:09:00 +0530

    new para added

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
 
 <h1>Building First PHP application<h1>
  <p1>pipeline is good option of the aws<p1>
+  <p2>wow at the time of creating beanstalk server into the cloud formation<p2>
 
 <?php
 echo "Version: 6.0";
